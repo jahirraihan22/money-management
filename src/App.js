ec1f007a0c95922da3ec87442fc5b2ddce4aadde
jraihan-me <jahirraihanmsd@gmail.com>
2021-07-10 12:16:35 +0600

    chart added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
+import React, { useState } from "react";
 import "./App.css";
 
 // components
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const items = [
-    {
-      id: "e1",
-      title: "Buy Pizza",
-      amount: 120.65,
-      date: new Date(2021, 7, 8),
-    },
-    {
-      id: "e2",
-      title: "Buy Pencil",
-      amount: 1210.65,
-      date: new Date(2021, 7, 8),
-    },
-    {
-      id: "e3",
-      title: "Vist Cox's bazar",
-      amount: 12021.65,
-      date: new Date(2021, 7, 8),
-    },
-    {
-      id: "e4",
-      title: "Buy Phone",
-      amount: 11220.65,
-      date: new Date(2021, 7, 8),
-    },
-  ];
+const DUMMY_DATA = [];
 
-  const addExpenseHandler = (expense) => {
-    console.log("app.js :>> ", expense);
+function App() {
+  const [items, setItems] = useState(DUMMY_DATA);
+  const addExpenseHandler = (item) => {
+    setItems((prevItems) => {
+      return [item, ...prevItems];
+    });
   };
   return (
     <div className="App">
